c0323db98bba7b14d8caeffc1eab535fad39ea16
zhangzl <zhangzl@i-counting.cn>
2018-06-03 19:07:54 +0800

    添加了stackdemo

---
+++ src/main/java/test/lind/javaLindDay/feignClientDemo/MockClient.java
@@ -0,0 +1,16 @@
+package test.lind.javaLindDay.feignClientDemo;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+
+/**
+ * 模拟其他服务.
+ */
+@Profile("!integTest")
+@FeignClient(name = "serviceName")
+public interface MockClient {
+  @GetMapping(path="/balanceSheet/{clientCode}")
+  String balanceSheet(String clientCode);
+}
